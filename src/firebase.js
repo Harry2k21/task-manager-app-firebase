60eba17090a2353ef558524f9729b5181ecd5cb8
Harry2k21 <88850029+Harry2k21@users.noreply.github.com>
2023-02-08 10:32:01 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCtZpcIMhKoCF58LOoVCG0VA_B4RmOSD3o",
-  authDomain: "todo-avs.firebaseapp.com",
-  projectId: "todo-avs",
-  storageBucket: "todo-avs.appspot.com",
-  messagingSenderId: "915034672734",
-  appId: "1:915034672734:web:3218f596e3a9b7f1572ecf",
-  measurementId: "G-DNYRMFLC01"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
